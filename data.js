d06bc0ebcd3e786e8b891081c702d2aeda6663f6
julia <juliathaiane2009@gmail.com>
2026-06-18 09:31:05 -0300

    Add player data for Brazilian football team

---
+++ data.js
@@ -0,0 +1,215 @@
+export const players = [
+  // Goleiros
+   { 
+    id: 1, 
+    pais: "Brasil", 
+    jogador: "Alisson Becker", 
+    posicao: "Goleiro", 
+    numero: 1, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRioP3Cjzz8Lyx1PiR8uHfrkyBzak2Kn86kOY9yOBW44A&s" 
+  },
+  { 
+    id: 2, 
+    pais: "Brasil", 
+    jogador: "Ederson", 
+    posicao: "Goleiro", 
+    numero: 23, 
+    avatar: "https://tntsports.com.br/__export/1703615067979/sites/esporteinterativo/img/2023/10/13/ederson_-_gettyimages-1677287656.jpg" 
+  },
+  { 
+    id: 3, 
+    pais: "Brasil", 
+    jogador: "Weverton", 
+    posicao: "Goleiro", 
+    numero: 12, 
+    avatar: "https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2022/12/06/2022-12-05t210204z_907147001_up1eic51mfeg0_rtrmadp_3_soccer-worldcup-bra-kor-report-ubu8lsovfwvp.JPG" 
+  },
+  // Defensores / Zagueiros e Laterais
+  { 
+    id: 4, 
+    pais: "Brasil", 
+    jogador: "Marquinhos", 
+    posicao: "Zagueiro", 
+    numero: 4, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHOEFGVTEw-aEUuFcj0n4ZyTXv8MtW2aG7PGryh23Gq9clFZQQShpRMlQ&s=10" 
+  },
+  { 
+    id: 5, 
+    pais: "Brasil", 
+    jogador: "Gabriel Magalhães", 
+    posicao: "Zagueiro", 
+    numero: 3, 
+    avatar: "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0910%2Fr1222832_683x384_16%2D9.jpg" 
+  },
+   { 
+    id: 6, 
+    pais: "Brasil", 
+    jogador: "Bremer", 
+    posicao: "Zagueiro", 
+    numero: 15, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK1wAlcwIZft7xoyqJ00EdPqdDSmZS9Oqc6SXP6CfTwA&s=10" 
+  },
+  { 
+    id: 7, 
+    pais: "Brasil", 
+    jogador: "Danilo", 
+    posicao: "Lateral", 
+    numero: 2, 
+    avatar: "https://veja.abril.com.br/wp-content/uploads/2022/12/GettyImages-1245389887.jpg?crop=1&resize=1212,909" 
+  },
+  { 
+    id: 8, 
+    pais: "Brasil", 
+    jogador: "Alex Sandro", 
+    posicao: "Lateral", 
+    numero: 6, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTe9QYt4msTda0MklVSFxuWdxH06x_4oAnPuN2KtKeew&s=10" 
+  },
+  { 
+    id: 9, 
+    pais: "Brasil", 
+    jogador: "Douglas Santos", 
+    posicao: "Lateral", 
+    numero: 16, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1st5PzKsZhB7Qg5oZOCWZjRD6p7XAnIkjkGUDcwhH5w&s=10" 
+  },
+  { 
+    id: 10, 
+    pais: "Brasil", 
+    jogador: "Ibañez", 
+    posicao: "Zagueiro", 
+    numero: 14, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfDGZGlDx1iRw4_CzB2LiJ_iYkxE3oy7Pov5k48V1I5g&s=10" 
+  },
+  { 
+    id: 11, 
+    pais: "Brasil", 
+    jogador: "Léo Pereira", 
+    posicao: "Zagueiro", 
+    numero: 25, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ4fFmrlE1gX5kzNh6NWHy0AENL05ZvGgdmbJP6stjMg&s=10" 
+  },
+
+  // Meio-campo
+  { 
+    id: 12, 
+    pais: "Brasil", 
+    jogador: "Casemiro", 
+    posicao: "Meio-campo", 
+    numero: 5, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZTL6ggHlJnQSVXzstAY93a4lc5pVyLnK217tEoYKwNw&s=10" 
+  },
+  { 
+    id: 13, 
+    pais: "Brasil", 
+    jogador: "Bruno Guimarães", 
+    posicao: "Meio-campo", 
+    numero: 8, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbG4HFyLNIwcN741WwFTMgcUILMmCyf3oZOim9YTV8iw&s=10" 
+  },
+  { 
+    id: 14, 
+    pais: "Brasil", 
+    jogador: "Lucas Paquetá", 
+    posicao: "Meio-campo", 
+    numero: 18, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRucXJBLlS7vOxMgvNWMQznEjKUv9FIrt6pdoY5MIXoJw&s=10" 
+  },
+  { 
+    id: 15, 
+    pais: "Brasil", 
+    jogador: "Fabinho", 
+    posicao: "Meio-campo", 
+    numero: 15, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQajR3NsuZtkM9Br43F7i08EPyP7iR31qBoDUT8qWjzIQ&s=10" 
+  },
+{ 
+    id: 16, 
+    pais: "Brasil", 
+    jogador: "Danilo Santos", 
+    posicao: "Meio-campo", 
+    numero: 20, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb3CTRPscqy-llDhXZHzHCj9CyZMPzAOMW9wme5PLXog&s=10" 
+  },
+  { 
+    id: 17, 
+    pais: "Brasil", 
+    jogador: "Éderson", 
+    posicao: "Meio-campo", 
+    numero: 26, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf1ERyz2ZNJgb9Kcf0Pkpd1EgB_z0H76NsuZPPHMALjw&s=10" 
+  },
+  // Atacantes
+  { 
+    id: 18, 
+    pais: "Brasil", 
+    jogador: "Neymar Jr.", 
+    posicao: "Atacante", 
+    numero: 10, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmkS92lxU4Bi2O-VSFpXvZDsWl5wx5-HFkt3ei4XyTmA&s=10" 
+  },
+  { 
+    id: 19, 
+    pais: "Brasil", 
+    jogador: "Vinícius Júnior", 
+    posicao: "Atacante", 
+    numero: 7, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVlp40rAJlzRHIexXGE78n2geYadygQ0hf4CWyXipqQg&s=10" 
+  },
+  { 
+    id: 20, 
+    pais: "Brasil", 
+    jogador: "Raphinha", 
+    posicao: "Atacante", 
+    numero: 19, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToQwJRLxsIsXX3YPO38eGPPvCYTqTBIflJZCG--4VC5Q&s=10" 
+  },
+  { 
+    id: 21, 
+    pais: "Brasil", 
+    jogador: "Endrick", 
+    posicao: "Atacante", 
+    numero: 9, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJrRzYHklBccoKttXL0Pky7JH6jdo_cDKV8xRFLgGing&s=10" 
+  },
+  { 
+    id: 22, 
+    pais: "Brasil", 
+    jogador: "Gabriel Martinelli", 
+    posicao: "Atacante", 
+    numero: 22, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeO1spLZ7PUMSHkR5LqhErkh2Be9Yw4aQUs3DCqbMYxg&s=10" 
+  },
+  { 
+    id: 23, 
+    pais: "Brasil", 
+    jogador: "Matheus Cunha", 
+    posicao: "Atacante", 
+    numero: 14, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYf_7Ov1ilj_6wIzWD3DP1QC7rzYLlXcpn5t1-GUFSHQ&s=10" 
+  },
+  { 
+    id: 24, 
+    pais: "Brasil", 
+    jogador: "Luiz Henrique", 
+    posicao: "Atacante", 
+    numero: 21, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBxvDym4lccLeaUthoWepCixy1tNQ9rvuJr-ho_VQc2Q&s=10" 
+  },
+  { 
+    id: 25, 
+    pais: "Brasil", 
+    jogador: "Igor Thiago", 
+    posicao: "Atacante", 
+    numero: 20, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfNM87kbJ71vAO-Q-g68pIdS3kjjq1Lqzyti3KZp9FgA&s=10" 
+  },
+  { 
+    id: 26, 
+    pais: "Brasil", 
+    jogador: "Rayan", 
+    posicao: "Atacante", 
+    numero: 26, 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6sf2pO10ctKeuAs0QRAnAyI9jmHmT9xSAztW6X16jTQ&s=10" 
+  }
+];
